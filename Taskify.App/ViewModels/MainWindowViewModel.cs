59e7e1b0ef92f2b5b639cebeb19191e27b47c8b3
Quentin <20quentin05@gmail.com>
2025-01-29 18:25:36 +0100

    Inputbinding to save

--- Taskify.App/ViewModels/MainWindowViewModel.cs
+++ Taskify.App/ViewModels/MainWindowViewModel.cs
@@ -1,5 +1,7 @@
-﻿using Prism.Mvvm;
+﻿using Prism.Commands;
+using Prism.Mvvm;
 using Prism.Regions;
+using Taskify.Modules.ToDo;
 using Taskify.Modules.ToDo.Views;
 
 namespace Taskify.App.ViewModels
@@ -13,9 +15,18 @@ namespace Taskify.App.ViewModels
             set { SetProperty(ref _title, value); }
         }
 
+        private DelegateCommand _windowClosingCommand;
+        public DelegateCommand WindowClosingCommand =>
+            _windowClosingCommand ??= new DelegateCommand(OnWindowClosing);
+
         public MainWindowViewModel(IRegionManager regionManager)
         {
             regionManager.RequestNavigate("ContentRegion", nameof(Taskify.Modules.ToDo.Views.TaskListView));
+        }
+
+        private void OnWindowClosing()
+        {
+            // Publish Event um im TaskListViewModel das speichern anzustoßen
         }
     }
 }
